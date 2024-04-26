66c53d977c3ebc272db856ba90ed07bd48f3c664
Lưu Lạc Ling <nguyenvantay061999@gmail.com>
2024-04-26 17:25:05 +0700

    TayNV-update

--- src/components/HomePage/Banner.js
+++ src/components/HomePage/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
     return (
 
         <div className="w-full h-[100vh] ph:max-md:h-[15rem] pv:max-ph:h-[13rem] pv:max-md:px-[12%] lg:h-fit relative">
-            <img src={BannerImg} alt="Healthy meal" className="w-full object-contain pv:max-md:h-fit pv:max-md:pl-0 pv:max-md:pt-0 pv:max-md:w-[20rem] 2xl:pr-40 pl-56 pt-16 h-[100vh] lg:h-fit lg:pt-0 lg:object-cover" />
+            <img src={BannerImg} alt="Healthy meal" className=" img_healthy w-full object-contain pv:max-md:h-fit pv:max-md:pl-0 pv:max-md:pt-0 pv:max-md:w-[20rem] 2xl:pr-40 pl-56 pt-16 h-[100vh] lg:h-fit lg:pt-0 lg:object-cover " />
             <div className="absolute z-40 top-0 h-[100vh] md:h-[25rem] lg:ml-[12%] pv:max-md:h-fit flex flex-col justify-center  2xl:ml-[20rem] xl:ml-60 2xl:pt-20 lg:pt-32 pv:max-md:mt-12">
                 <p className="font-nexa_light text-left 2xl:text-[70px] lg:text-[50px] 2xl:leading-[68px] lg:leading-[45px] pv:max-md:leading-[16px]  ">DON'T <br></br> EAT LESS. <br></br>JUST <br></br>EAT REAL.</p>
                 <button className="bg-[#B2CC60] pv:max-md:text-[7px] pv:max-md:p-[10px] px-6 py-3 rounded-full font-shopee_bold mt-12 pv:max-md:mt-4 ">ORDER CATHERING ONLINE</button>
