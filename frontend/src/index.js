a85fee9b89334e577536ad2ca2734c85f8939aab
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2024-05-12 16:28:43 +0700

    h

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TagManager from 'react-gtm-module'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-K79HF867',
+  auth: 'W1M5jYd6-mA03DuobURe9Q&gtm_preview=env-1'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
